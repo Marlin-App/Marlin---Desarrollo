5a2d58eaa31a0a7536863f2e4bfd0f359b9b7692
JoiJaf <jafethrocha11@gmail.com>
2024-09-11 22:09:47 -0600

    home

--- FrontEnd/screens/Home.js
+++ FrontEnd/screens/Home.js
@@ -6,34 +6,41 @@ import AntDesign from '@expo/vector-icons/AntDesign';
 import Feather from '@expo/vector-icons/Feather';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
 
-
 export function HomeScreen({ navigation }) {
 
     const verticalData = [
         {
-            id: '1', title: 'Categorias', horizontalData: [
+            id: '1',
+            title: 'Categorias',
+            type: 'category',
+            horizontalData: [
                 { id: '1a', title: '' },
                 { id: '1b', title: '' },
-                { id: '1c', title: '' }
+                { id: '1c', title: '' },
+                { id: '1d', title: '' }
             ]
         },
         {
-            id: '2', title: 'Productos para ti', horizontalData: [
+            id: '2',
+            title: 'Productos para ti',
+            type: 'product',
+            horizontalData: [
                 { id: '2a', title: '' },
                 { id: '2b', title: '' },
                 { id: '2c', title: '' }
             ]
         },
         {
-            id: '3', title: 'Tiendas destacadas', horizontalData: [
-                { id: '2a', title: '' },
-                { id: '2b', title: '' },
-                { id: '2c', title: '' }
+            id: '3',
+            title: 'Tiendas destacadas',
+            type: 'store',
+            horizontalData: [
+                { id: '3a', title: '' },
+                { id: '3b', title: '' },
+                { id: '3c', title: '' }
             ]
-        },
-
+        }
     ];
-
 
     const renderHorizontalItem = ({ item }) => (
         <View className="bg-gray-200 p-5 my-2 mx-2 rounded-lg w-40 h-40">
@@ -41,52 +48,28 @@ export function HomeScreen({ navigation }) {
         </View>
     );
 
+    const renderHorizontalC = ({ item }) => (
+        <View className="bg-gray-200 p-5 my-2 mx-2 rounded-lg w-20 h-20">
+            <Text className="text-lg">{item.title}</Text>
+        </View>
+    );
+
     const renderVerticalItem = ({ item }) => (
-        <View className=" p-2 my-2">
-            <Text className="ml-8 mt-2 text-4xl font-semibold text-blue-950">{item.title}</Text>
+        <View className="p-2 my-2">
+            <Text className="ml-8 mt-2 text-[28px] font-extrabold text-main-blue">{item.title}</Text>
             <FlatList
                 data={item.horizontalData}
-                renderItem={renderHorizontalItem}
+                renderItem={item.type === 'category' ? renderHorizontalC : renderHorizontalItem}
                 keyExtractor={horizontalItem => horizontalItem.id}
                 horizontal
                 showsHorizontalScrollIndicator={false}
             />
         </View>
     );
-    /* useEffect(() => {
-        navigation.setOptions({
-
-
-            headerSearchBarOptions: {
-                headerShown: true,
-                placeholder: 'Buscar',
-                styles: {
-                    backgroundColor: '#0038A2',
-                    color: 'white',
-                    height: 50,
-                    borderRadius: 10,
-                    fontSize: 20,
-                    width: 300,
-                    margin: 10,
-                    padding: 10,
-                },
-            },
-            headerSearchBarStyle: {
-                backgroundColor: '#0038A2',
-                color: 'white',
-                height: 50,
-                borderRadius: 10, 
-                fontSize: 20,
-                width: 300,
-                margin: 10,
-                padding: 10,
-            },
-        });
-    }, []); */
 
     return (
         <View className="flex-1">
-            <View className=" w-full flex-col pl-8 pr-8 bg-main-blue pt-6 pb-4 " >
+            <View className="w-full flex-col pl-8 pr-8 bg-main-blue pt-14 pb-4">
                 <View className="flex-row justify-between w-full">
                     <View className="flex-row items-center">
                         <Text className="text-white text-lg">Carr. Interamericana Norte</Text>
@@ -94,13 +77,18 @@ export function HomeScreen({ navigation }) {
                     </View>
                     <Feather name="shopping-bag" size={24} color="white" />
                 </View>
-                <View className="flex-row  mt-5 bg-white rounded-lg">
-                    <View className="bg-light-blue rounded-l-lg">
+                <View className="flex-row mt-9 rounded-lg">
+                    <View className="bg-light-blue rounded-l-lg p-2">
                         <MaterialCommunityIcons name="magnify" size={30} color="white" />
                     </View>
-                    <TextInput className="ml-2 w-full "
+                    <TextInput
+                        className="mr-2 bg-white w-64 rounded-r-lg pl-4"
                         placeholder='Buscar'
+                        placeholderTextColor={"#88B1FF"}
                     />
+                    <View className="bg-white rounded-lg p-2">
+                        <AntDesign name="filter" size={28} color="#60a5fa" />
+                    </View>
                 </View>
             </View>
             <View>
