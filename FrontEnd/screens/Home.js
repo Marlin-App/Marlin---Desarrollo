82d603121dbad4b33f237648e4502f55735bfb8d
JoiJaf <jafethrocha11@gmail.com>
2024-09-11 22:59:15 -0600

    Carruseles del home

--- FrontEnd/screens/Home.js
+++ FrontEnd/screens/Home.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Button, Text, TextInput, View, StatusBar, FlatList, RefreshControl } from 'react-native';
+import { Button, Text, TextInput, View, StatusBar, FlatList, RefreshControl, Image } from 'react-native';
 import { useEffect, useCallback } from 'react';
 import { styled } from 'nativewind';
 import AntDesign from '@expo/vector-icons/AntDesign';
@@ -10,8 +10,6 @@ import * as SplashScreen from 'expo-splash-screen';
 
 export function HomeScreen({ navigation }) {
 
-
-
     const [fontsLoaded] = useFonts({
         'Excon_regular': require('../../FrontEnd/assets/fonts/Excon/Excon-Regular.otf'),
         'Excon_bold': require('../../FrontEnd/assets/fonts/Excon/Excon-Bold.otf'),
@@ -19,9 +17,6 @@ export function HomeScreen({ navigation }) {
         'Erode_regular': require('../../FrontEnd/assets/fonts/Erode/Erode-Regular.otf'),
         'Erode_bold': require('../../FrontEnd/assets/fonts/Erode/Erode-Bold.otf')
     });
-
-
-    
 
     useEffect(() => {
         async function prepare() {
@@ -36,10 +31,7 @@ export function HomeScreen({ navigation }) {
         }
     }, [fontsLoaded])
 
-    if (!fontsLoaded)
-        return null;
-
-
+    if (!fontsLoaded) return null;
 
     const verticalData = [
         {
@@ -47,10 +39,10 @@ export function HomeScreen({ navigation }) {
             title: 'Categorias',
             type: 'category',
             horizontalData: [
-                { id: '1a', title: '' },
-                { id: '1b', title: '' },
-                { id: '1c', title: '' },
-                { id: '1d', title: '' }
+                { id: '1a', title: '', image: require('../../FrontEnd/assets/img/fondoLanding.png') },
+                { id: '1b', title: '', image: require('../../FrontEnd/assets/img/fondoLanding.png') },
+                { id: '1c', title: '', image: require('../../FrontEnd/assets/img/fondoLanding.png') },
+                { id: '1d', title: '', image: require('../../FrontEnd/assets/img/fondoLanding.png') }
             ]
         },
         {
@@ -58,9 +50,13 @@ export function HomeScreen({ navigation }) {
             title: 'Productos para ti',
             type: 'product',
             horizontalData: [
-                { id: '2a', title: '' },
-                { id: '2b', title: '' },
-                { id: '2c', title: '' }
+                { id: '2a', title: '', image: require('../../FrontEnd/assets/img/fondoLanding.png') },
+                { id: '2b', title: '', image: require('../../FrontEnd/assets/img/fondoLanding.png') },
+                { id: '2c', title: '', image: require('../../FrontEnd/assets/img/fondoLanding.png') },
+                { id: '2d', title: '', image: require('../../FrontEnd/assets/img/fondoLanding.png') },
+                { id: '2e', title: '', image: require('../../FrontEnd/assets/img/fondoLanding.png') },
+                { id: '2f', title: '', image: require('../../FrontEnd/assets/img/fondoLanding.png') },
+                { id: '2g', title: '', image: require('../../FrontEnd/assets/img/fondoLanding.png') }
             ]
         },
         {
@@ -68,28 +64,38 @@ export function HomeScreen({ navigation }) {
             title: 'Tiendas destacadas',
             type: 'store',
             horizontalData: [
-                { id: '3a', title: '' },
-                { id: '3b', title: '' },
-                { id: '3c', title: '' }
+                { id: '3a', title: '', image: require('../../FrontEnd/assets/img/fondoLanding.png') },
+                { id: '3b', title: '', image: require('../../FrontEnd/assets/img/fondoLanding.png') },
+                { id: '3c', title: '', image: require('../../FrontEnd/assets/img/fondoLanding.png') }
             ]
         }
     ];
 
     const renderHorizontalItem = ({ item }) => (
-        <View className="bg-red-800 p-5 my-2 mx-2 rounded-lg w-40 h-40">
+        <View className="bg-cyan-600 my-2 mx-2 rounded-lg w-40 h-40">
+            <Image
+                source={item.image}
+                className="w-full h-full rounded-lg"
+                resizeMode="cover"
+            />
             <Text className="text-lg">{item.title}</Text>
         </View>
     );
 
     const renderHorizontalC = ({ item }) => (
         <View className="bg-gray-200 p-5 my-2 mx-2 rounded-lg w-20 h-20">
+            <Image
+                source={item.image}
+                className="w-full h-12 rounded-lg"
+                resizeMode="cover"
+            />
             <Text className="text-lg">{item.title}</Text>
         </View>
     );
 
     const renderVerticalItem = ({ item }) => (
-        <View className=" p-2 my-2" onLayout={onLayout}>
-            <Text className="ml-2 mt-2 text-2xl font-Excon_bold">{item.title}</Text>
+        <View className="p-2 my-2" onLayout={onLayout}>
+            <Text className="ml-2 mt-2 text-2xl font-Excon_bold text-main-blue">{item.title}</Text>
             <FlatList
                 data={item.horizontalData}
                 renderItem={item.type === 'category' ? renderHorizontalC : renderHorizontalItem}
@@ -102,7 +108,7 @@ export function HomeScreen({ navigation }) {
 
     return (
         <View className="flex-1">
-            <View className=" w-full flex-col pl-8 pr-8 bg-main-blue pt-8 pb-6 " >
+            <View className="w-full flex-col pl-8 pr-8 bg-main-blue pt-14 pb-4">
                 <View className="flex-row justify-between w-full">
                     <View className="flex-row items-center">
                         <Text className="text-white text-lg font-Excon_regular">Carr. Interamericana Norte</Text>
@@ -110,11 +116,12 @@ export function HomeScreen({ navigation }) {
                     </View>
                     <Feather name="shopping-bag" size={24} color="white" />
                 </View>
-                <View className="flex-row text-center mt-5 bg-white rounded-lg">
-                    <View className="bg-light-blue rounded-l-lg px-2 flex justify-center">
+                <View className="flex-row mt-9 rounded-lg">
+                    <View className="bg-light-blue rounded-l-lg p-2">
                         <MaterialCommunityIcons name="magnify" size={30} color="white" />
                     </View>
-                    <TextInput className="ml-2 py-4 w-full text-md font-Erode_regular"
+                    <TextInput
+                        className="mr-2 bg-white w-64 rounded-r-lg pl-4"
                         placeholder='Buscar'
                         placeholderTextColor={"#88B1FF"}
                     />
