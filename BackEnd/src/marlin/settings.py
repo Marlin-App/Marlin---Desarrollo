c0dfb9ed63e1dd05e44829fc35958fc12fb8b5d6
Jeranguz <JeremyGD997@hotmail.com>
2024-10-06 00:54:18 -0600

    ajuste 2

--- BackEnd/src/marlin/settings.py
+++ BackEnd/src/marlin/settings.py
@@ -209,7 +209,7 @@ cloudinary.config(
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static')
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
