b87fa82c05f625fc5047dbc835ad8c15edaa21d3
DrexGuzman <drexjesusguzman@gmail.com>
2024-10-06 22:45:20 -0600

    useCrudProductos.js, hace fetch de los productos de cada tienda y los productos, tambien borra productos de cada tienda

---
+++ FrontEnd/hooks/useCRUDProductos.js
@@ -0,0 +1,134 @@
+import { useEffect, useState } from "react";
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import useDecodeJWT from './useDecodeJWT';
+
+
+export function useCRUDProductos() {
+
+
+    const [storesWithProducts, setStoresWithProducts] = useState([]);
+    const { decodeJWT } = useDecodeJWT();
+
+    const fetchStoresWithProducts = async () => {
+        try {
+            // Obtén el token almacenado en AsyncStorage
+            const jsonValue = await AsyncStorage.getItem('@userToken');
+
+            if (!jsonValue) {
+                setLoading(false);
+                setIsLogged(false);
+                return;
+            }
+
+            const userData = JSON.parse(jsonValue);
+            const token = userData.access;
+            const decodedToken = decodeJWT(token);
+            const user_id = decodedToken.payload.userprofile;
+
+            // Fetch para obtener las tiendas del usuario
+            const response = await fetch(`https://marlin-backend.vercel.app/api/stores/?user_id=${user_id}`, {
+                headers: {
+                    'Authorization': `Bearer ${token}`
+                }
+            });
+
+            if (!response.ok) {
+                throw new Error('Error fetching stores');
+            }
+
+            const stores = await response.json();
+
+            // Mapeamos las tiendas para agregar sus productos
+            const storesWithProductsPromises = stores.map(async (store) => {
+                try {
+                    const productsResponse = await fetch(`https://marlin-backend.vercel.app/api/storeItems/?store_id=${store.id}`, {
+                        headers: {
+                            'Authorization': `Bearer ${token}`
+                        }
+                    });
+
+                    if (!productsResponse.ok) {
+                        console.error(`Error fetching products for store ${store.id}`);
+                        return { ...store, products: [] }; // Si falla, se asigna un array vacío
+                    }
+
+                    const products = await productsResponse.json();
+                    return { ...store, products: Array.isArray(products) ? products : [] };
+
+                } catch (error) {
+                    console.error(`Error fetching products for store ${store.id}:`, error);
+                    return { ...store, products: [] }; // En caso de error, asignar un array vacío
+                }
+            });
+
+            const storesWithProducts = await Promise.all(storesWithProductsPromises);
+
+            // Establecemos las tiendas con sus productos en el estado
+            setStoresWithProducts(storesWithProducts);
+
+        } catch (err) {
+            console.error('Error fetching stores with products:', err);
+            setError(err);
+        } finally {
+            setLoading(false);
+        }
+    };
+
+
+
+
+
+
+    const [idStore, setIdStore] = useState(3);
+    const [productLista, setProductLista] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    const fecthProductList = async () => {
+
+        try {
+            const response = await fetch(`https://marlin-backend.vercel.app/api/storeItems/?store_id=${idStore}`);
+            const json = await response.json();
+            setProductLista(json);
+        } catch (err) {
+            setError(err);
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    const deleteProduct = async (id) => {
+
+        const jsonValue = await AsyncStorage.getItem('@userToken');
+        if (jsonValue == null) {
+            setLoading(false);
+            setIsLogged(false);
+            return;
+        }
+        const userData = JSON.parse(jsonValue);
+        const token = userData.access;
+
+        try {
+            const response = await fetch(`https://marlin-backend.vercel.app/api/storeItems/${id}/`, {
+                method: 'DELETE',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${token}`
+                }
+            });
+            if (!response.ok) {
+                const errorData = await response.json();
+                console.log('Error de respuesta:', errorData);
+                return;
+            }
+            console.log('Producto eliminado exitosamente');
+            fecthProductList();
+        } catch (err) {
+            console.log('Error al eliminar el producto:', err);
+        }
+    };
+
+
+    return { fecthProductList, productLista, deleteProduct, fetchStoresWithProducts, storesWithProducts };
+
+}
--- FrontEnd/screens/ComercianteInventario.js
+++ FrontEnd/screens/ComercianteInventario.js
@@ -1,18 +1,24 @@
 import { Text, View, Button, Item, FlatList, TextInput, SafeAreaView, SectionList, Pressable, Image, ScrollView, ActivityIndicator, TouchableOpacity, Modal } from 'react-native';
 import Ionicons from "@expo/vector-icons/Ionicons";
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import AntDesign from '@expo/vector-icons/AntDesign';
-
-
-
+import { useCRUDProductos } from '../hooks/useCRUDProductos';
 
 
 export function ComercianteInventario({ navigation }) {
 
     const [modalVisible, setModalVisible] = useState(false);
+    const { fecthProductList, productLista, deleteProduct, fetchStoresWithProducts, storesWithProducts } = useCRUDProductos();
+    const [longPressProduct, setLongPressProduct] = useState(null);
 
-    const longpress = () => {
 
+    useEffect(() => {
+        fetchStoresWithProducts();
+        //fecthProductList();
+    }, []);
+
+    const longpress = (id) => {
+        setLongPressProduct(id);
         setModalVisible(true);
     }
     return (
@@ -31,105 +37,68 @@ export function ComercianteInventario({ navigation }) {
             </View>
             <Text className="text-2xl text-main-blue font-Excon_bold mt-4 ml-4">Productos de tus tiendas</Text>
             <ScrollView horizontal={true}>
-                <View className="flex-col p-4 w-[100vw]">
-                    <Text className="text-lg text-main-blue font-Excon_bold">The Fresh Market</Text>
-                    <Text className="text-md font-Excon_thin text-main-blue">Puntarenas, El roble</Text>
-                    <View className="flex-col">
-                        <View className="items-end mb-4">
-                            <Pressable className="border-main-blue border-[1.5px] rounded-full w-7 h-7 justify-center items-center" onPress={() => navigation.navigate("AgregarProducto")}>
-                                <Ionicons name="add" size={24} color="#015DEC" />
-                            </Pressable>
-                        </View>
-                        <View className="border-[0.5px] border-main-blue rounded-lg px-4 py-2">
-                            <ScrollView className="h-[50vh]">
-                                <Pressable className="flex-row justify-center mb-1" onPress={() => navigation.navigate("EditarProducto")} onLongPress={longpress}>
-                                    {/* Foto con imnformacion */}
-                                    <View className="flex-row justify-between m-2 px-4 py-4 border-[0.5px] border-main-blue w-full items-center rounded-md">
-                                        {/* Contenedor de la imagen y la información del producto */}
-                                        <View className="flex-row w-[95%] max-w-full">
-                                            {/* Imagen del producto */}
-                                            <View className="justify-center items-center bg-red-600 rounded-lg">
-                                                <Image
-                                                    source={require('../assets/img/marlin.png')}
-                                                    style={{ width: 100, height: 100, resizeMode: "contain" }}
-                                                />
-                                            </View>
+                {storesWithProducts.map((store, storeIndex) => (
+                    <View key={storeIndex} className="flex-col p-4 w-[100vw]">
+                        <Text className="text-lg text-main-blue font-Excon_bold">{store.name}</Text>
+                        <Text className="text-md font-Excon_thin text-main-blue">{store.canton}, {store.district}</Text>
+                        <View className="flex-col">
+                            <View className="items-end mb-4">
+                                <Pressable className="border-main-blue border-[1.5px] rounded-full w-7 h-7 justify-center items-center" onPress={() => navigation.navigate("AgregarProducto")}>
+                                    <Ionicons name="add" size={24} color="#015DEC" />
+                                </Pressable>
+                            </View>
+                            <View className="border-[0.5px] border-main-blue rounded-lg px-4 py-2">
+                                <FlatList className="h-[50vh]"
+                                    data={store.products}
+                                    keyExtractor={(item, index) => index.toString()}
+                                    renderItem={({ item, index }) => (
+                                        <Pressable
+                                            key={index}
+                                            className="flex-col justify-center mb-1"
+                                            onPress={() => navigation.navigate("EditarProducto")}
+                                            onLongPress={() => longpress(index)}
+                                        >
+                                            <View className="flex-row justify-between mt-2 px-4 py-4 border-[0.5px] border-main-blue w-full items-center rounded-md">
 
-                                            {/* Información del producto */}
-                                            <View className="px-6 flex-shrink">
-                                                <Text className="font-Excon_bold text-sm">Titulo del producto</Text>
-                                                <Text className="font-Excon_thin text-sm" numberOfLines={3}>
-                                                    Este es un producto de la mejor calidad y alta resistencia a golpes
-                                                </Text>
-                                                <View className="justify-between flex-row">
-                                                    <Text className="font-Excon_thin text-sm">₡12 000</Text>
-                                                    <Text className="font-Excon_bold text-sm">Inventario: <Text className="font-Excon_thin text-sm">30</Text></Text>
+                                                <View className="flex-row w-[95%] max-w-full">
+
+                                                    <View className="justify-center items-center">
+                                                        <Image
+                                                            className="rounded-lg"
+                                                            source={{ uri: item.picture }}
+                                                            style={{ width: 100, height: 100 }}
+                                                        />
+                                                    </View>
+
+                                                    <View className="px-6 flex-shrink">
+                                                        <Text className="font-Excon_bold text-sm">{item.name}</Text>
+                                                        <Text className="font-Excon_thin text-sm" numberOfLines={3}>
+                                                            {item.description}
+                                                        </Text>
+                                                        <View className="justify-between flex-row">
+                                                            <Text className="font-Excon_thin text-sm">₡{item.price}</Text>
+                                                            <Text className="font-Excon_bold text-sm">
+                                                                Inventario: <Text className="font-Excon_thin text-sm">{item.inventory}</Text>
+                                                            </Text>
+                                                        </View>
+                                                    </View>
+                                                </View>
+
+                                                <View className="flex-col gap-y-1 h-full items-center">
+                                                    <View className="bg-main-blue rounded-full w-[5px] h-[5px]"></View>
+                                                    <View className="bg-main-blue rounded-full w-[5px] h-[5px]"></View>
+                                                    <View className="bg-main-blue rounded-full w-[5px] h-[5px]"></View>
                                                 </View>
                                             </View>
-                                        </View>
-
-                                        {/* 3 Puntos (opciones adicionales) */}
-                                        <View className="flex-col gap-y-1 h-full items-center">
-                                            <View className="bg-main-blue rounded-full w-[5px] h-[5px]"></View>
-                                            <View className="bg-main-blue rounded-full w-[5px] h-[5px]"></View>
-                                            <View className="bg-main-blue rounded-full w-[5px] h-[5px]"></View>
-                                        </View>
-                                    </View>
-                                </Pressable>
-                            </ScrollView>
+                                        </Pressable>
+                                    )}
+                                    showsVerticalScrollIndicator={false}
+                                    contentContainerStyle={{ paddingBottom: 20 }}
+                                />
+                            </View>
                         </View>
                     </View>
-                </View>
-
-                <View className="flex-col p-4 w-[100vw]">
-                    <Text className="text-lg text-main-blue font-Excon_bold">The Fresh Market</Text>
-                    <Text className="text-md font-Excon_thin text-main-blue">Puntarenas, El roble</Text>
-                    <View className="flex-col">
-                        <View className="items-end mb-4">
-                            <Pressable className="border-main-blue border-[1.5px] rounded-full w-7 h-7 justify-center items-center" onPress={() => navigation.navigate("AgregarProducto")}>
-                                <Ionicons name="add" size={24} color="#015DEC" />
-                            </Pressable>
-                        </View>
-                        <View className="border-[0.5px] border-main-blue rounded-lg px-4 py-2">
-                            <ScrollView className="h-[50vh]">
-                                <Pressable className="flex-row justify-center mb-1" onPress={() => navigation.navigate("EditarProducto")} onLongPress={longpress}>
-                                    {/* Foto con imnformacion */}
-                                    <View className="flex-row justify-between m-2 px-4 py-4 border-[0.5px] border-main-blue w-full items-center rounded-md">
-                                        {/* Contenedor de la imagen y la información del producto */}
-                                        <View className="flex-row w-[95%] max-w-full">
-                                            {/* Imagen del producto */}
-                                            <View className="justify-center items-center bg-red-600 rounded-lg">
-                                                <Image
-                                                    source={require('../assets/img/marlin.png')}
-                                                    style={{ width: 100, height: 100, resizeMode: "contain" }}
-                                                />
-                                            </View>
-
-                                            {/* Información del producto */}
-                                            <View className="px-6 flex-shrink">
-                                                <Text className="font-Excon_bold text-sm">Titulo del producto</Text>
-                                                <Text className="font-Excon_thin text-sm" numberOfLines={3}>
-                                                    Este es un producto de la mejor calidad y alta resistencia a golpes
-                                                </Text>
-                                                <View className="justify-between flex-row">
-                                                    <Text className="font-Excon_thin text-sm">₡12 000</Text>
-                                                    <Text className="font-Excon_bold text-sm">Inventario: <Text className="font-Excon_thin text-sm">30</Text></Text>
-                                                </View>
-                                            </View>
-                                        </View>
-
-                                        {/* 3 Puntos (opciones adicionales) */}
-                                        <View className="flex-col gap-y-1 h-full items-center">
-                                            <View className="bg-main-blue rounded-full w-[5px] h-[5px]"></View>
-                                            <View className="bg-main-blue rounded-full w-[5px] h-[5px]"></View>
-                                            <View className="bg-main-blue rounded-full w-[5px] h-[5px]"></View>
-                                        </View>
-                                    </View>
-                                </Pressable>
-                            </ScrollView>
-                        </View>
-                    </View>
-                </View>
+                ))}
             </ScrollView>
 
             {modalVisible && (
@@ -144,14 +113,15 @@ export function ComercianteInventario({ navigation }) {
                     <View className="flex-1 justify-center items-center">
                         <View className="bg-white border-[1.5px]  rounded-lg p-6 shadow-lg w-[80vw]">
                             <View className="justify-center items-center">
-                            <AntDesign name="warning" size={70} color="black" />
-                            <Text className="text-lg text-main-blue font-Excon_bold mb-4">¿Deseas eliminar este producto?</Text>
+                                <AntDesign name="warning" size={70} color="black" />
+                                <Text className="text-lg text-main-blue font-Excon_bold mb-4">¿Deseas eliminar este producto?</Text>
                             </View>
                             <View className="flex-row justify-between">
                                 <TouchableOpacity
                                     className="border-[0.5px] border-main-blue rounded-lg px-4 py-2 "
                                     onPress={() => {
-                                        // Lógica para eliminar el producto
+                                        deleteProduct(longPressProduct);
+                                        setLongPressProduct(null);
                                         setModalVisible(false);
                                     }}
                                 >
@@ -159,7 +129,10 @@ export function ComercianteInventario({ navigation }) {
                                 </TouchableOpacity>
                                 <TouchableOpacity
                                     className="bg-main-blue rounded-lg px-4 py-2"
-                                    onPress={() => setModalVisible(false)}
+                                    onPress={() => {
+                                        setLongPressProduct(null)
+                                        setModalVisible(false);
+                                    }}
                                 >
                                     <Text className="text-white font-Excon_regular">Cancelar</Text>
                                 </TouchableOpacity>
