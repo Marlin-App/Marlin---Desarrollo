8bc9871ac90eac11f88cb0e629201e9d5fd75603
Luisda1828 <LUIS.VILLEGASHERRERA@ucr.ac.cr>
2024-10-19 15:08:28 -0600

    Avances en HomeComerciante

--- FrontEnd/screens/ComerciantePedidoScreen.js
+++ FrontEnd/screens/ComerciantePedidoScreen.js
@@ -4,22 +4,31 @@ import React, { useState } from 'react';
 import Feather from '@expo/vector-icons/Feather';
 import Entypo from '@expo/vector-icons/Entypo';
 import AntDesign from '@expo/vector-icons/AntDesign';
+import {useRoute} from '@react-navigation/native';
 
 export function ComerciantePedidoScreen() {
     const { colorScheme } = useColorScheme();
 
     const [modalVisible, setModalVisible] = useState(false);
+    const [modalProduct, setModalProduct] = useState(null);
+    const {order} = useRoute().params;
+    console.log(order);
 
     return (
         <ScrollView className="bg-white dark:bg-neutral-950 h-full px-5">
-            <View className="flex-col border-b-[0.5px] pb-3 dark:border-light-blue">
+            <View className="flex-col border-b-[0.5px] pb-3 dark:border-light-blue ">
 
-                <Text className="text-md font-Excon_regular text-main-blue mt-8 ml-4 dark:text-light-blue">Cliente</Text>
-                <Text className="text-xl font-Excon_bold text-main-blue ml-4 dark:text-light-blue">Gustavo Acosta Fernandez</Text>
-                <Text className="text-md font-Excon_thin mt-4 ml-4 dark:text-white">Fecha: 05/10/24</Text>
-                <Text className="text-md font-Excon_thin mt-2 ml-4 dark:text-white">Recibo:215415</Text>
-                <Text className="text-lg font-Excon_regular mt-2 ml-4 dark:text-white">Total: ₡215.415</Text>
-                {/* <Image className="bg-red-200" source={require('../assets/img/marlin.png')} style={{ width: 100, height: 100, resizeMode: "contain" }} /> */}
+                <View className="flex-row mt-8 ml-4 justify-between" >
+                    <View className="justify-end">
+                        <Text className="text-md font-Excon_regular text-main-blue   dark:text-light-blue">Cliente</Text>
+                        <Text className="text-xl font-Excon_bold text-main-blue  dark:text-light-blue">{order.customer.name} </Text>
+                    </View>
+                    <Image className="bg-red-200" source={{uri: order.logo }} style={{ width: 70, height: 70, resizeMode: "cover" }} />
+
+                </View>
+                <Text className="text-md font-Excon_thin mt-4 ml-4 dark:text-white">Fecha: {order.customer.date} </Text>
+                <Text className="text-md font-Excon_thin mt-2 ml-4 dark:text-white">Recibo: {order.receipt}</Text>
+                <Text className="text-lg font-Excon_regular mt-2 ml-4 dark:text-white">Total: {order.total}</Text>
             </View>
 
             <View className="flex-col mt-5 border-b-[0.5px] pb-6 dark:border-light-blue">
@@ -33,28 +42,34 @@ export function ComerciantePedidoScreen() {
                         <Text className="text-md font-Excon_bold text-main-blue">Cant.</Text>
                         <Text className="text-md font-Excon_bold text-main-blue">Subtotal</Text>
                     </View>
-                    <View className="flex-row justify-between mt-2">
-                        <Pressable onPress={() => setModalVisible(true)}>
-                            <Text className="text-md font-Excon_thin dark:text-white">Producto 1</Text>
-                        </Pressable>
-                        <Text className="text-md font-Excon_thin dark:text-white">₡100</Text>
-                        <Text className="text-md font-Excon_thin dark:text-white">2</Text>
-                        <Text className="text-md font-Excon_thin dark:text-white">₡200</Text>
-                    </View>
+
+                        {order.orderDetails.map((product, index) => (
+                            <View className="flex-row justify-between w-full mt-2">
+                            <Pressable onPress={() =>{setModalVisible(true); setModalProduct(product)}
+                                
+                            }>
+                            <Text className="text-md font-Excon_thin dark:text-white">{product.productName}</Text>
+                            </Pressable>
+                            <Text className="text-md font-Excon_thin dark:text-white">₡{product.price}</Text>
+                            <Text className="text-md font-Excon_thin dark:text-white">{product.quantity}</Text>
+                            <Text className="text-md font-Excon_thin dark:text-white">₡{product.price * product.quantity}</Text>
+                            </View>
+                            ))
+                            
+                            }
+                  
                     
                 </View>
 
-                {/* <Image className="bg-red-200" source={require('../assets/img/marlin.png')} style={{ width: 100, height: 100, resizeMode: "contain" }} /> */}
             </View>
             <Text className="text-xs font-Excon_thin mt-5 ml-4 dark:text-white">* Puedes hacer click en el nombre del producto para mayor información.</Text>
 
             <View className="flex-col mt-5 pb-6">
 
-                <Text className="text-xl font-Excon_bold text-main-blue ml-4 dark:text-light-blue">Entrega:</Text>
 
                 <View className="flex-col mt-5 ml-4">
-                    <Text className="text-main-blue text-md font-Excon_bold">Referencias</Text>
-                    <Text className="my-2 font-Excon_thin dark:text-white">Lorem ipsum dolor sit amet consectetur. Nec sed vel phasellus tincidunt dignissim consectetur. Sed imperdiet purus ultricies in faucibus non cursus blandit pellentesque. Et integer sollicitudin aliquam dictumst. Hac facilisis ac gravida ut porttitor pretium dui tristique et.</Text>
+                    <Text className="text-xl font-Excon_bold text-main-blue dark:text-light-blue">Referencias para la entrega</Text>
+                    <Text className="my-2 font-Excon_thin dark:text-white">{order.deliveryInfo.references} </Text>
                 </View>
 
                 {/* <Image className="bg-red-200" source={require('../assets/img/marlin.png')} style={{ width: 100, height: 100, resizeMode: "contain" }} /> */}
@@ -83,11 +98,11 @@ export function ComerciantePedidoScreen() {
                     <View className="flex-1 justify-center items-center bg-white dark:bg-neutral-950 p-10">
                     <Text className="text-main-blue text-2xl font-Excon_bold mb-10 dark:text-light-blue">Información del producto</Text>
                         <Image 
-                            source={{ uri: 'https://via.placeholder.com/150' }} 
+                            source={{ uri: modalProduct.productPicture }} 
                             style={{ width: 200, height: 200, resizeMode: 'contain' }} 
                         />
-                        <Text className="text-main-blue text-2xl font-Excon_bold mt-4 dark:text-light-blue">Producto 1</Text>
-                        <Text className="text-md font-Excon_thin mt-2 mb-10 dark:text-white">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam.</Text>
+                        <Text className="text-main-blue text-2xl font-Excon_bold mt-4 dark:text-light-blue">{modalProduct.productName}</Text>
+                        <Text className="text-md font-Excon_thin mt-2 mb-10 dark:text-white">{modalProduct.description}</Text>
                         <Button title="Cerrar" onPress={() => setModalVisible(false)} />
                     </View>
                 </Modal>
