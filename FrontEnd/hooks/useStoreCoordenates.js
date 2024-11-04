eaac7f62cea725aecdd7aa684b04d4bfcd8d0b03
DrexGuzman <drexjesusguzman@gmail.com>
2024-11-03 22:52:19 -0600

    usando la api de google directions para calcular la distancia entre la tienda y el punto de entrega, para estimar el costo de transporte.

---
+++ FrontEnd/hooks/useStoreCoordenates.js
@@ -0,0 +1,24 @@
+import { useState } from 'react';
+
+const useStoreCordenates = () => {
+
+    const [storeLatitude, setStoreLatitude] = useState(null);
+    const [storeLongitude, setStoreLongitude] = useState(null);
+
+    const getStoreCordenates = async (id) => {
+        try {
+            const response = await fetch(`https://marlin-backend.vercel.app/api/stores/${id}/`);
+            console.log(response);
+            const json = await response.json();
+            const coordinates = json.coodernates.split(',');
+            setStoreLatitude(parseFloat(coordinates[0]));
+            setStoreLongitude(parseFloat(coordinates[1]));
+        } catch (err) {
+            console.error('Error al obtener la tienda:', err);
+            return null;
+        }
+    }
+
+    return { getStoreCordenates, storeLatitude, storeLongitude };
+};
+export default useStoreCordenates;
---
+++ FrontEnd/hooks/useTransportFee.js
@@ -0,0 +1,32 @@
+export const useTransportFee = async (startCoords, endCoords) => {
+    const apiKey = 'AIzaSyB0fbhtl6sU-fzxajDD1-e5uD0f0XYrVpI';
+    const url = `https://maps.googleapis.com/maps/api/directions/json?origin=${startCoords.latitude},${startCoords.longitude}&destination=${endCoords.latitude},${endCoords.longitude}&key=${apiKey}`;
+  
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+  
+      if (data.routes.length) {
+        const distance = data.routes[0].legs[0].distance.text;
+        const distanceValue = data.routes[0].legs[0].distance.value;
+  
+        // Obtener las coordenadas de la ruta
+        const steps = data.routes[0].legs[0].steps;
+        const routeCoordinates = steps.map(step => ({
+          latitude: step.start_location.lat,
+          longitude: step.start_location.lng,
+        }));
+        routeCoordinates.push({
+          latitude: endCoords.latitude,
+          longitude: endCoords.longitude,
+        });
+  
+        return { distance, distanceValue, routeCoordinates };
+      } else {
+        throw new Error('No se encontró una ruta entre los puntos especificados.');
+      }
+    } catch (error) {
+      console.error('Error al obtener la distancia:', error.message || error);
+      return null;
+    }
+  };
